234f8a6426842818a85648f1888470c5ffd3b618
RyabovNA <kulek19862009@mail.ru>
2024-08-21 23:59:43 +0500

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -1,7 +1,9 @@
 def single_root_words(root_word, *other_words):
+    root_word_lower = root_word.lower()
     same_words = []
     for word in other_words:
-        if root_word in other_words or other_words == root_word:
+        word_lower = word.lower()
+        if root_word_lower in word_lower or word_lower in root_word_lower:
             same_words.append(word)
     return same_words
 
